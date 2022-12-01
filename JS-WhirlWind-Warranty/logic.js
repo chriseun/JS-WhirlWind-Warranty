552ab6837cb0c2fae2202f9f8b110c1e91a26d09
Christopher Eun <64549181+chriseun@users.noreply.github.com>
2022-12-01 10:12:36 -0800

    Add files via upload

---
+++ JS-WhirlWind-Warranty/logic.js
@@ -0,0 +1,194 @@
+function createWarrantyPage() {
+
+  document.body.style.backgroundColor = 'pink'
+
+
+let element = document.createElement('h2')
+element.textContent = 'Warranty Information'
+document.body.append(element)
+
+let label = document.createElement('label')
+label.textContent = 'First Name: '
+document.body.appendChild(label)
+
+let input = document.createElement('input')
+input.type = 'text'
+document.body.appendChild(input)
+
+let br = document.createElement('br')
+document.body.appendChild(br)
+
+let label2 = document.createElement('label')
+label2.textContent = 'Last Name: '
+document.body.appendChild(label2)
+
+let input2 = document.createElement('input')
+input2.type = 'text'
+document.body.appendChild(input2)
+
+let br2 = document.createElement('br')
+document.body.appendChild(br2)
+
+let label3 = document.createElement('label')
+label3.textContent = 'Company Name: '
+document.body.appendChild(label3)
+
+let input3 = document.createElement('input')
+input3.type = 'text'
+document.body.appendChild(input3)
+
+let br3 = document.createElement('br')
+document.body.appendChild(br3)
+
+let label4 = document.createElement('label')
+label4.textContent = 'Email: '
+document.body.appendChild(label4)
+
+let input4 = document.createElement('input')
+input4.type = 'text'
+document.body.appendChild(input4)
+
+let br4 = document.createElement('br')
+document.body.appendChild(br4)
+
+let label5 = document.createElement('label')
+label5.textContent = 'Phone Number: '
+document.body.appendChild(label5)
+
+let input5 = document.createElement('input')
+input5.type = 'text'
+document.body.appendChild(input5)
+
+let br5 = document.createElement('br')
+document.body.appendChild(br5)
+
+let label6 = document.createElement('label')
+label6.textContent = 'Fax Number: '
+document.body.appendChild(label6)
+
+let input6 = document.createElement('input')
+input6.type = 'text'
+document.body.appendChild(input6)
+
+let br6 = document.createElement('br')
+document.body.appendChild(br6)
+
+
+let label7 = document.createElement('label')
+label7.textContent = 'Address: '
+document.body.appendChild(label7)
+
+let input7 = document.createElement('input')
+input7.type = 'text'
+document.body.appendChild(input7)
+
+let br7 = document.createElement('br')
+document.body.appendChild(br7)
+
+let label8 = document.createElement('label')
+label8.textContent = 'City: '
+document.body.appendChild(label8)
+
+let input8 = document.createElement('input')
+input8.type = 'text'
+document.body.appendChild(input8)
+
+let br8 = document.createElement('br')
+document.body.appendChild(br8)
+
+let label9 = document.createElement('label')
+label9.textContent = 'State: '
+document.body.appendChild(label9)
+
+let input9= document.createElement('input')
+input9.type = 'text'
+document.body.appendChild(input9)
+
+let br9 = document.createElement('br')
+document.body.appendChild(br9)
+
+let label10 = document.createElement('label')
+label10.textContent = 'Zip: '
+document.body.appendChild(label10)
+
+let input10 = document.createElement('input')
+input10.type = 'text'
+document.body.appendChild(input10)
+
+let br10 = document.createElement('br')
+document.body.appendChild(br10)
+
+let label11 = document.createElement('label')
+label11.textContent = 'Country: '
+document.body.appendChild(label11)
+
+let input11 = document.createElement('input')
+input10.type = 'text'
+document.body.appendChild(input11)
+
+let br11 = document.createElement('br')
+document.body.appendChild(br11)
+
+let label12 = document.createElement('label')
+label12.textContent = 'Unit Information: '
+document.body.appendChild(label12)
+
+let input12 = document.createElement('input')
+input12.type = 'text'
+document.body.appendChild(input12)
+
+let br12 = document.createElement('br')
+document.body.appendChild(br12)
+
+
+let label13 = document.createElement('label')
+label13.textContent = 'Date of Purchase: '
+document.body.appendChild(label13)
+
+let input13 = document.createElement('input')
+input13.type = 'text'
+document.body.appendChild(input13)
+
+let br13 = document.createElement('br')
+document.body.appendChild(br13)
+
+let label14 = document.createElement('label')
+label14.textContent = 'Model: '
+document.body.appendChild(label14)
+
+let input14 = document.createElement('input')
+input14.type = 'text'
+document.body.appendChild(input14)
+
+let br14 = document.createElement('br')
+document.body.appendChild(br14)
+
+let label15 = document.createElement('label')
+label15.textContent = 'Warranty Serial Number: '
+document.body.appendChild(label15)
+
+let input15 = document.createElement('input')
+input15.type = 'text'
+document.body.appendChild(input15)
+
+let br15 = document.createElement('br')
+document.body.appendChild(br15)
+
+let button = document.createElement('button')
+button.textContent = 'Save'
+document.body.appendChild(button)
+
+let button1 = document.createElement('button')
+button1.textContent = 'Close'
+document.body.appendChild(button1)
+
+
+// document.createElement('br')
+// document.body.appendChild(br)
+
+
+
+
+}
+
+createWarrantyPage()
